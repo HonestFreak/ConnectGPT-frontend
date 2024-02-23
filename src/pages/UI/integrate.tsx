6ac9b3cf745f0d8f40bb0e8750c7542fa91649ed
honestfreak <me21b006@iittp.ac.in>
2024-02-24 02:37:13 +0530

    loading

--- src/pages/UI/integrate.tsx
+++ src/pages/UI/integrate.tsx
@@ -125,7 +125,22 @@ const Integrate = () => {
 
 </div>
     </>
-  );
+  )
+  else {
+    return (
+      <div class='flex space-x-2 justify-center items-center h-screen'>
+          <div class='h-8 w-8 rounded-full animate-bounce [animation-delay:-0.3s]'>
+            🔴
+          </div>
+          <div class='h-8 w-8 rounded-full animate-bounce [animation-delay:-0.15s]'>
+            🟡
+          </div>
+          <div class='h-8 w-8 rounded-full animate-bounce'>
+            🔵
+          </div>
+      </div>
+    )
+  }
 };
 
 export default Integrate;
